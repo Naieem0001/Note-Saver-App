93e5fcbe919f71a07960467551dd8fa62d0b5902
Naieem Qureshi <naieemq111@gmail.com>
2026-03-25 15:38:02 +0530

    fixed variables in serverjs and env files

--- server.js
+++ server.js
@@ -12,15 +12,16 @@ app.get('/', (req, res) => {
 
 // Connect to MySQL
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
 
 db.getConnection((err, connection) => {
   if (err) {
